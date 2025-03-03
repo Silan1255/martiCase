cb242f1d64bfc90f6892e12ebc7eecc2134cb895
Silan <boransilan907@gmail.com>
2025-03-03 22:37:32 +0300

    edited add marker distance

--- app/src/main/java/com/example/martiproject/ui/map/MapContract.kt
+++ app/src/main/java/com/example/martiproject/ui/map/MapContract.kt
@@ -4,7 +4,7 @@ object MapContract {
     const val API_KEY = "AIzaSyC5fF77MLj8LDmurhnFPM4_lZRC0mJ_eEE"
     const val CACHE_SIZE = 100
     const val LOCATION_PERMISSION_REQUEST_CODE = 1001
-    const val MIN_DISTANCE_TO_ADD_MARKER = 1f
+    const val MIN_DISTANCE_TO_ADD_MARKER = 10f
     const val GEOCODING_API_URL = "https://maps.googleapis.com/maps/api/geocode/json"
     const val DIRECTIONS_API_URL = "https://maps.googleapis.com/maps/api/directions/json"
     const val NOTIFICATION_ID = 12345
